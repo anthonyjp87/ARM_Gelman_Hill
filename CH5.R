e0e27287794039d4394384baa1353696ce279eaa
anthonyjp87 <john.a.perez@gmail.com>
2016-03-18 17:11:44 +0000

    Ch5 commit

--- CH5.R
+++ CH5.R
@@ -1,3 +1,7 @@
 #rodents
 
-rodents <- read.table("rodents.dat")
+r <- read.table("rodents.dat")
+fit.1 <- glm(rodent2~dilap, family=binomial, data=r)
+display(fit.1)
+plot(jitter(dilap, factor=.5), jitter(rodent2, factor=.5), pch=20)
+curve (invlogit(fit.1$coef[1] + fit.1$coef[2]*x),xlim=c(-.2,1.2), add=TRUE)
