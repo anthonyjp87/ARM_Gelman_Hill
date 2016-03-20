4d5c48a7e7e07c0624b02870462195221dd362b8
anthonyjp87 <john.a.perez@gmail.com>
2016-03-20 15:54:07 +0000

    Updates to Ch5

--- CH5.R
+++ CH5.R
@@ -1,7 +1,14 @@
 #rodents
 
 r <- read.table("rodents.dat")
-fit.1 <- glm(rodent2~dilap, family=binomial, data=r)
+r.training <- r[1:1000,]
+r.test <- r[1001:1747,]
+print(sapply(r,function(x) sum(is.na(x))))
+print("unique")
+print(sapply(r, function(x) length(unique(x))))
+# missmap(r,main="missing data")
+
+fit.1 <- glm(rodent2~dilap, family=binomial, data=r.training)
 display(fit.1)
 plot(jitter(dilap, factor=.5), jitter(rodent2, factor=.5), pch=20)
 curve (invlogit(fit.1$coef[1] + fit.1$coef[2]*x),xlim=c(-.2,1.2), add=TRUE)
