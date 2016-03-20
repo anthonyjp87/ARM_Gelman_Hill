69f404c3812303b2aac8c60ebf17822ca3fabd27
anthonyjp87 <john.a.perez@gmail.com>
2016-03-20 17:26:45 +0000

    predict & Check

--- CH5.R
+++ CH5.R
@@ -14,3 +14,17 @@ plot(jitter(dilap, factor=.5), jitter(rodent2, factor=.5), pch=20)
 curve (invlogit(fit.1$coef[1] + fit.1$coef[2]*x),xlim=c(-.2,1.2), add=TRUE)
 print(anova(fit.1, test="Chisq"))
 print(pR2(fit.1))
+
+
+fitted.results <- predict(fit.1,newdata=r.test,type='response')
+fitted.results <- ifelse(fitted.results > 0.5,1,0)
+misclas <-c(0)
+
+r.test$rodent2[is.na(r.test$rodent2)] <- 0
+
+for(n in 1:length(fitted.results)){
+  misclas[n]<-ifelse(fitted.results[n]!=r.test$rodent2[n],0,1)
+}
+
+predictionrate<-(1-(length(which(misclas==0))/(length(which(misclas==1)))))
+
